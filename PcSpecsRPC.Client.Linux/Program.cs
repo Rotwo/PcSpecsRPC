edba8287e540666f4548c8a1a7f5ca2b4ca31be4
Rotwo <87866939+Rotwo@users.noreply.github.com>
2025-09-21 19:39:02 +0200

    Check for roots permission

--- PcSpecsRPC.Client.Linux/Program.cs
+++ PcSpecsRPC.Client.Linux/Program.cs
@@ -2,8 +2,17 @@
 
 internal class Program
 {
+    public static bool IsAdministrator => Mono.Unix.Native.Syscall.geteuid() == 0;
+
     private static void Main(string[] args)
     {
+        if (IsAdministrator is not true)
+        {
+            Console.WriteLine("This program must be runned as root!");
+            Console.WriteLine("Please try to run again using sudo.");
+            throw new SystemException("Program must be runned as root");
+        }
+
         var systemInfoProvider = new LinuxSystemInfoProvider();
         var cpuInfo = systemInfoProvider.GetCpuInfo();
 
@@ -14,5 +23,15 @@ internal class Program
         Console.WriteLine(cpuInfo.Threads);
         Console.WriteLine(cpuInfo.BaseClockGHz);
         Console.WriteLine(cpuInfo.MaxClockGHz);
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        var ramInfo = systemInfoProvider.GetRamInfo();
+        Console.WriteLine(ramInfo.TotalMemoryGb);
+        Console.WriteLine(ramInfo.TotalMemoryMb);
     }
 }
