b87ec4d589a2716dd22786b6715932b75321f47d
Rotwo <87866939+Rotwo@users.noreply.github.com>
2025-09-21 19:38:47 +0200

    Create function to transform from GB to MB

--- PcSpecsRPC.Client.Common/Utils/SystemParserUitls.cs
+++ PcSpecsRPC.Client.Common/Utils/SystemParserUitls.cs
@@ -4,16 +4,29 @@
     {
         public static int? ExtractNumbers(string? input)
         {
-            if (input is null) return null;
+            if (string.IsNullOrWhiteSpace(input)) return null;
+            if (input.Equals("None", StringComparison.OrdinalIgnoreCase)) return null;
 
             string digitsOnly = new string(input.Where(char.IsDigit).ToArray());
-            int ramMb = int.Parse(digitsOnly);
-            return ramMb;
+            
+            if (string.IsNullOrEmpty(digitsOnly)) return null;
+            
+            if (int.TryParse(digitsOnly, out int result))
+            {
+                return result;
+            }
+            
+            return null;
         }
 
         public static int? MbToGb(int input)
         {
             return (int)Math.Floor((double)(input / 1024.0));
+        }
+
+        public static int GbToMb(int input)
+        {
+            return input * 1024;
         }
     }
 }
