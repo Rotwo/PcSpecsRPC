f4a924c08e0a54863a65093290d065578fe4693f
Rotwo <rotwodeveloper@gmail.com>
2025-09-21 19:49:35 +0200

    Modify look of response and add typing (temporal ui)

--- frontend/pc-specs-rpc-web/src/routes/index.tsx
+++ frontend/pc-specs-rpc-web/src/routes/index.tsx
@@ -7,9 +7,58 @@ export const Route = createFileRoute('/')({
   component: App,
 })
 
+interface PcSpecs {
+  clientId: string
+  specs: {
+    cpuInfo: {
+      name: string
+      cores: number
+      threads: number
+      baseClockGHz: number
+      maxClockGHz: number
+    }
+    osInfo: {
+      name: string
+      version: string
+      manufacturer: string
+      architecture: string
+    }
+    displayDeviceInfo: Array<{
+      name: string
+      driverVersion: string
+      manufacturer: string
+      dedicatedMemory: string
+      videoProcessor: string | null
+      deviceId: string
+      currentDisplayMode: string
+      monitor: {
+        name: string
+        model: string
+        id: string
+        nativeMode: string
+        outputType: string
+      }
+    }>
+    ramInfo: {
+      totalMemoryMb: number
+      totalMemoryGb: number
+    }
+    soundDeviceInfo: Array<{
+      description: string
+      hardwareId: string
+      driverName: string
+      driverVersion: string
+      driverProvider: string
+      driverDate: string
+      defaultSoundPlayback: number
+      defaultVoicePlayback: number
+    }>
+  }
+}
+
 function App() {
-  const [pcSpecs, setPcSpecs] = useState()
-  const [clientId, setClientId] = useState()
+  const [pcSpecs, setPcSpecs] = useState<PcSpecs | undefined>()
+  const [clientId, setClientId] = useState<string | undefined>()
 
   SignalRContext.useSignalREffect(
     'ReceiveClientId',
@@ -23,8 +72,8 @@ function App() {
   SignalRContext.useSignalREffect(
     'ReceivePcSpecs',
     (content) => {
-      console.log(content)
-      setPcSpecs(content)
+      console.log(content as PcSpecs)
+      setPcSpecs(content as PcSpecs)
     },
     [],
   )
@@ -32,14 +81,22 @@ function App() {
   return (
     <div className="p-4">
       <h1>Your client id is: {clientId}</h1>
-      <p>Data received: {pcSpecs !== undefined ? 'True!' : 'false.'}</p>
+      <p>Data received: {pcSpecs?.clientId != null ? 'Ready!' : 'Not'}</p>
       <div>
-        <textarea
-          name="data"
-          id="data"
-          className="w-full p-4 h-max bg-gray-300 rounded-lg mt-8"
-          value={pcSpecs != undefined ? JSON.stringify(pcSpecs as string) : ''}
-        ></textarea>
+        {pcSpecs && (
+          <pre
+            lang="json"
+            style={{
+              textAlign: 'left',
+              background: '#f4f4f4',
+              padding: '1em',
+              borderRadius: '8px',
+              overflowX: 'auto',
+            }}
+          >
+            {JSON.stringify(pcSpecs, null, 2)}
+          </pre>
+        )}
       </div>
     </div>
   )
